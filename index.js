59cb176ff12bb976af58c975402894fae72b3fab
Kamal Relwani <kamal.relwani@bookmyshow.com>
2018-01-19 18:29:20 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-var app = express();
+const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello Heroku App!');
