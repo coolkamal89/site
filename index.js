b0b18a2c17c3db5069d41c04d3ecdd2426fea333
Kamal Relwani <kamal.relwani@bookmyshow.com>
2018-01-19 19:37:25 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	const date = new Date().toString();
