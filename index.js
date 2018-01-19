520bb7a1d8dca005c3238857bf030759247f95a7
Kamal Relwani <kamal.relwani@bookmyshow.com>
2018-01-19 19:12:35 +0530

    Minor changes

--- index.js
+++ index.js
@@ -14,12 +14,30 @@ app.get('/hook', (req, res) => {
 });
 
 app.post('/hook', (req, res) => {
-	console.log(req.body);
-	res.send({
-		success: true
-	});
+	if (req.body.message) {
+		var message = req.body.message;
+		var message_id = message.message_id;
+		var chat_id = message.chat.id;
+
+		if (message.text) {
+			var text = message.text;
+			var response = apiRequestWebhook('sendMessage', { chat_id: chat_id, text: 'Hello' });
+			
+			res.send(response);
+		}
+	}
 });
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
 });
+
+function apiRequestWebhook(method, parameters) {
+	if (!parameters) {
+		parameters = {};
+	}
+
+	parameters.method = method;
+
+	return parameters;
+}
