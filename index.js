a286b77bef3bb36a371512f5e7069a1b30f1128f
Kamal Relwani <kamal.relwani@bookmyshow.com>
2018-01-19 20:17:54 +0530

    Minor changes

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+var quizEntries = [
+	{ q: "What is 1 + 1?", a1: "", a2: "", a3: "", a4: "", c: "ans1" }
+];
 
+function getQues(chat_id) {
+	return quizEntries[0];
+}
+
+const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -23,7 +30,30 @@ app.post('/hook', (req, res) => {
 				res.send({ method: 'sendMessage', chat_id: chat_id, text: 'This is the help screen.' });
 			}
 
-			res.send({ method: 'sendMessage', chat_id: chat_id, text: 'Hello' });
+			else if (text.indexOf('/start/') >= 0) {
+				var ques = getQues(chat_id);
+
+				var ques_form = ques.q;
+				ques_form += '1. ' + ques.a1 + '\n';
+				ques_form += '2. ' + ques.a2 + '\n';
+				ques_form += '3. ' + ques.a3 + '\n';
+				ques_form += '4. ' + ques.a4 + '\n';
+
+				res.send({
+					method: 'sendMessage',
+					chat_id: chat_id,
+					text: 'Let\'s start with the quiz.\n\n' + ques_form,
+					reply_markup: {
+				        'keyboard' => [['1', '2', '3', '4']],
+				        'one_time_keyboard': true,
+				        'resize_keyboard': true
+				    }
+				});
+			}
+
+			else {
+				res.send({ method: 'sendMessage', chat_id: chat_id, text: 'Hello' });
+			}
 		}
 	}
 });
