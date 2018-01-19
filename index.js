b71e99140e3ed166016896e053139378a2c8ba8c
Kamal Relwani <kamal.relwani@bookmyshow.com>
2018-01-19 18:34:50 +0530

    Minor changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello Heroku App!');
+	res.send('Hello Heroku App! The current time is ${new Date().toString()}');
 });
 
 app.listen(PORT, () => {
