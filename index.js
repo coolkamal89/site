ed8b346e6c0b5a1d555bc1792df8ab6a148176a6
Kamal Relwani <coolkamal89@yahoo.co.in>
2020-09-21 20:39:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,20 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/stocks/quote/:symbol', (req, res) => {
-	https.get('https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=' + req.params.symbol, (httpsRes) => {
+	const options = {
+		hostname: 'www1.nseindia.com',
+		port: 443,
+		path: '/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=' + req.params.symbol,
+		method: 'GET',
+		headers: {
+			'Accept': '*/*',
+			'Accept-Language': 'en-US,en;q=0.5',
+			'Host': 'www1.nseindia.com',
+			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+			'X-Requested-With': 'XMLHttpRequest'
+		}
+	};
+	https.request(options, (httpsRes) => {
 		// console.log('statusCode:', httpsRes.statusCode);
 		// console.log('headers:', httpsRes.headers);
 
